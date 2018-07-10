d79ad2a940a33710116acbd58a7ee2eb456b91ec
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-10 09:07:11 +0530

    Set uup the player character and the arm rotation

---
+++ 2D Platformer/Assets/Scripts/ArmRotation.cs
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour
+{	
+	public int rotationOffset = 0;
+
+	void Update()
+	{
+		Vector3 difference = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		difference.Normalize();							//normalizing the vector
+		float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; //find the angle in degrees
+		transform.rotation = Quaternion.Euler(0, 0, rotZ + rotationOffset);
+	}
+}
