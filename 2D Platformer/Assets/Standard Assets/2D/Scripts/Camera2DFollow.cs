b988046e7415ba50017136b2f87f729e55a7c682
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-11 19:14:48 +0530

    Clapmed the camera on the y axis

--- 2D Platformer/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ 2D Platformer/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+        public float yPosRestriction = -1;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -45,6 +46,7 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
+            newPos = new Vector3 (newPos.x, Mathf.Clamp(newPos.y, yPosRestriction, Mathf.Infinity), newPos.z);
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
