33e0fdf7043d17b65feb73aad885213bc64d7c12
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-13 23:15:52 +0530

    Set up Enemy AI

---
+++ 2D Platformer/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,125 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Seeker))]
+public class EnemyAI : MonoBehaviour
+{
+	//what to chase
+	public Transform target;
+	
+	//how many times we will update our path per second
+	public float updateRate = 2f;
+
+	//caching
+	private Seeker _seeker;
+	private Rigidbody2D _rb;
+
+	//the calculated path
+	public Path path;
+
+	//the AI's speed per second
+	public float speed =  300f;
+
+	public ForceMode2D fMode;
+
+	[HideInInspector]
+	public bool pathHasEnded = false;
+
+	//the max distance from the AI to a waypoint for it to continue to the next waypoint
+	public float nextWaypointDistance = 3f;
+
+	//waypoint we are currently moving towards
+	private int _currentWaypoint;
+
+	void Start()
+	{
+		_seeker = GetComponent<Seeker>();
+		_rb = GetComponent<Rigidbody2D>();
+
+		if(target == null)
+		{
+			Debug.Log("No player found.");
+			return;
+		}
+
+		//start a new path to the target position, return the result to the OnComplete method
+		_seeker.StartPath(transform.position, target.position, onPathComplete);
+
+		StartCoroutine(UpdatePath());
+	}
+
+	public void onPathComplete(Path p)
+	{
+		Debug.Log("Did it have an error? " + p.error);
+
+		if(!p.error)
+		{
+			path = p;
+
+			_currentWaypoint = 0;
+		}
+	}
+
+	public IEnumerator UpdatePath()
+	{
+		if(target == null)
+		{
+			yield return false;
+		}
+
+		//start a new path to the target position, return the result to the OnComplete method
+		_seeker.StartPath(transform.position, target.position, onPathComplete);
+
+		yield return new WaitForSeconds( 1f/updateRate);
+
+		StartCoroutine(UpdatePath());
+	}
+
+	void FixedUpdate()
+	{
+		if(target == null)
+		{
+			return;
+		}
+
+		//TODO: always look at player
+
+		if(_currentWaypoint >= path.vectorPath.Count)
+		{
+			if(pathHasEnded)
+			{
+				return;
+			}
+
+			Debug.Log("End of path reached.");
+			pathHasEnded = true;
+			return;
+		}
+
+		pathHasEnded = false;
+
+		//direction to the next waypoint
+		Vector3 dir = (path.vectorPath[_currentWaypoint] - transform.position).normalized;
+
+		dir *= speed*Time.fixedDeltaTime;
+
+		//move the AI
+		_rb.AddForce(dir, fMode);
+
+		float dist = Vector3.Distance(transform.position, path.vectorPath[_currentWaypoint]);
+		if(dist < nextWaypointDistance)
+		{
+			_currentWaypoint++;
+			return;
+		}
+	}
+
+}
