477e3fa393a103f5e2a599311e46d9385112f973
joyDivided13 <40304211+joyDivided13@users.noreply.github.com>
2018-07-14 18:49:08 +0530

    Added enemy GUI

--- 2D Platformer/Assets/Scripts/Enemy.cs
+++ 2D Platformer/Assets/Scripts/Enemy.cs
@@ -12,18 +12,50 @@ public class Enemy : MonoBehaviour
 	[System.Serializable]
 	public class EnemyStats
 	{
-		public int health = 100;
+		public int maxHealth = 100;
+		private int _curHealth;
+
+		public int curHealth //getter, setter
+		{
+			get{ return _curHealth;}
+			set{ _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+		}
+
+		public void init()
+		{
+			curHealth = maxHealth;
+		}
 	}
 
-	public EnemyStats stats = new EnemyStats();		//an instance of the PlayerStats class
+	
+	public EnemyStats stats = new EnemyStats();
+
+	[Header("Optional:")]
+	[SerializeField]
+	private StatusIndicator statusIndicator;
+		
+	void Start()
+	{
+		stats.init();
+
+		if(statusIndicator != null)
+		{
+			statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+		}
+	}
 
 	public void DamageEnemy(int damage)
 	{
-		stats.health -= damage;
+		stats.curHealth -= damage;
 
-		if(stats.health <= 0)
+		if(stats.curHealth <= 0)
 		{
 			GameManager.instance.KillEnemy(this);
+		}
+
+		if(statusIndicator != null)
+		{
+			statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
 		}
 	}
 }
---
+++ 2D Platformer/Assets/Scripts/StatusIndicator.cs
@@ -0,0 +1,37 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour
+{
+	[SerializeField]
+	private RectTransform healthBarRect;
+	[SerializeField]
+	private Text healthText;
+
+	void Start()
+	{
+		if(healthBarRect == null)
+		{
+			Debug.LogError("NO STATUS INDICATOR bar object referenced.");
+		}
+		if(healthText == null)
+		{
+			Debug.LogError("NO STATUS INDICATOR text object referenced.");
+		}
+	}
+
+	public void SetHealth(int _cur, int _max)
+	{
+		float _value = (float)_cur/_max;
+
+		healthBarRect.localScale = new Vector3(_value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+		healthText.text = _cur + "/" + _max + " HP";
+	}
+}
